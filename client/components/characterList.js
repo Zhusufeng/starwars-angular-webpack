30c4d1f985a21996d82a48f70bbfbd4eee366637
Lisa Gee <zhusufeng@gmail.com>
2017-12-16 21:41:40 -0800

    Set template based on location of index.html

--- client/components/characterList.js
+++ client/components/characterList.js
@@ -1,6 +1,5 @@
-angular.module('starwars')
-  .component('characterList', {
-    controller: 'charController',
-    templateUrl: 'components/views/char.html'
-    // template: "<h1>Star Wars Character Names</h1>"
-  });
+angular.module('starwars').component('characterList', {
+  controller: 'charController',
+  templateUrl: 'client/components/views/char.html'
+  // template: "<h1>Star Wars Character Names</h1>"
+});
