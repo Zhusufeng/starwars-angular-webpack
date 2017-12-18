2b6053d7ecd74f7d796702adccdeb5e1d63eb149
Lisa Gee <zhusufeng@gmail.com>
2017-12-17 23:42:42 -0800

    Require dependencies for app.js instead of in index.html

--- client/app.js
+++ client/app.js
@@ -1,1 +1,9 @@
-angular.module('starwars', ['ui.router'])
+const angular = require('angular');
+const uirouter = require('angular-ui-router');
+
+angular.module('starwars', [uirouter])
+
+require('./routes');
+require('./components');
+require('./controllers');
+require('./services');
