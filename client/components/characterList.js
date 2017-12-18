c0bed78207ec7a6834cd12a748a1f09525fb1235
Lisa Gee <zhusufeng@gmail.com>
2017-12-18 00:05:19 -0800

    Debug name in template and controller in quotes

--- client/components/characterList.js
+++ client/components/characterList.js
@@ -1,5 +1,5 @@
 module.exports = {
-  template: require('./views/characterList.html'),
-  controller: charController
+  template: require('./views/char.html'),
+  controller: 'charController'
   // template: "<h1>Star Wars Character Names</h1>"
 }
