34a65d3e25aabddcf51511c4f33d7ff9d390d1bb
Lisa Gee <zhusufeng@gmail.com>
2017-12-18 00:21:05 -0800

    Add strict, change export style in charService

--- client/services/charService.js
+++ client/services/charService.js
@@ -1,8 +1,12 @@
+'use strict';
+
 charService.$inject = ['$http'];
 
-export default function charService($http) {
+function charService($http) {
   this.getCharacters = function() {
     console.log('charService is running...');
     return $http.get('https://swapi.co/api/people');
   }
 }
+
+module.exports = charService;
