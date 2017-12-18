f5c8c5170231f7fd3153cb54e1aa780c60764a3c
Lisa Gee <zhusufeng@gmail.com>
2017-12-17 23:53:57 -0800

    Refactor charController to comply with webpack

--- client/controllers/charController.js
+++ client/controllers/charController.js
@@ -1,8 +1,10 @@
-angular.module('starwars').controller('charController', function (charService) {
+charController.$inject = ['charService'];
+export default function charController (charService) {
   this.characters = [];
+  
   charService.getCharacters()
     .then(res => {
       console.log('You received these results from charService', res.data.results)
       this.characters = res.data.results;
     });
-});
+}
