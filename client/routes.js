cf561a2b35e643d501e475313907d68393a73858
Lisa Gee <zhusufeng@gmail.com>
2017-12-17 23:47:17 -0800

    Refactor routes to comply with webpack

--- client/routes.js
+++ client/routes.js
@@ -1,11 +1,10 @@
-angular.module('starwars')
-  .config(function ($stateProvider, $urlRouterProvider) {
+routes.$inject = ['$stateProvider', '$urlRouterProvider']
+export default function routes ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      template: '<character-list></character-list>'
+    });
 
-    $stateProvider
-      .state('home', {
-        url: '/',
-        template: '<character-list></character-list>'
-      });
-  
-    $urlRouterProvider.otherwise('/');
-  })
+  $urlRouterProvider.otherwise('/');
+}
