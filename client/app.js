98eb5a3f77ddfae6d03172bfc8a771658bda02b3
Lisa Gee <zhusufeng@gmail.com>
2017-12-18 00:27:07 -0800

    Move requirements around in app.js

--- client/app.js
+++ client/app.js
@@ -1,9 +1,10 @@
 const angular = require('angular');
 const uirouter = require('angular-ui-router');
 const routes = require('./routes');
-require('./components');
-require('./controllers');
-require('./services');
 
 angular.module('starwars', [uirouter])
   .config(routes);
+
+require('./components');
+require('./controllers');
+require('./services');
