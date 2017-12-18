c87d957557874bd71c0255627a89dd7af40c613c
Lisa Gee <zhusufeng@gmail.com>
2017-12-17 23:48:30 -0800

    Move config to app.js

--- client/app.js
+++ client/app.js
@@ -1,9 +1,9 @@
 const angular = require('angular');
 const uirouter = require('angular-ui-router');
-
-angular.module('starwars', [uirouter])
-
 require('./routes');
 require('./components');
 require('./controllers');
 require('./services');
+
+angular.module('starwars', [uirouter])
+  .config(routes);
